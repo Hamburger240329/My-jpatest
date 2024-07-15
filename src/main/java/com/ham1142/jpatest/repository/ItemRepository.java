d9da3602d7cec0c99ba52176c673e2ba4afcfe9e
JBEDU29 <5514cyj@gmail.com>
2024-07-15 16:18:27 +0900

    각종 조회 테스트

--- src/main/java/com/ham1142/jpatest/repository/ItemRepository.java
+++ src/main/java/com/ham1142/jpatest/repository/ItemRepository.java
@@ -3,5 +3,14 @@ package com.ham1142.jpatest.repository;
 import com.ham1142.jpatest.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
+
+    List<Item> findByItemNm(String itemNm);
+
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail); // 이름이나 Detail 둘 중 하나만 일치하는 필드 값을 조회
+
+    List<Item> findByPriceLessThanEqual(int price);
+
 }
--- src/test/java/com/ham1142/jpatest/ItemRepositoryTest.java
+++ src/test/java/com/ham1142/jpatest/ItemRepositoryTest.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
 public class ItemRepositoryTest {
@@ -18,21 +20,53 @@ public class ItemRepositoryTest {
     @Test
     @DisplayName("상품 저장 테스트")
     public void createItemTest() {
-        Item item = new Item();
-        item.setItemNm("테스트 상품");
-        item.setPrice(10000);
-        item.setItemDetail("테스트 상품 상세 설명");
-        item.setStockNumber(100);
-        item.setRegTime(LocalDateTime.now()); // 현재 시간으로 삽입
-        item.setUpdateTime(LocalDateTime.now());
+        for(int i=1; i<=10;i++){
+            Item item = new Item();
+            item.setItemNm("테스트 상품" + i);
+            item.setPrice(10000 + i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setStockNumber(100);
+            item.setRegTime(LocalDateTime.now()); // 현재 시간으로 삽입
+            item.setUpdateTime(LocalDateTime.now());
 
-        Item savedItem = itemRepository.save(item);
-        System.out.println(savedItem.toString());
+            Item savedItem = itemRepository.save(item);
+            System.out.println(savedItem.toString());
+        }
+
 
     }
 
     @Test
-    @DisplayName("상품명조회");
-    public void findByIt
+    @DisplayName("상품명 조회 테스트")
+    public void findByItemNmTest() {
+        List<Item> itemList = itemRepository.findByItemNm("테스트 상품"); // item 이름으로 DB 필드 값 찾기
+
+        for(Item item:itemList){
+            System.out.println(item.toString());
+        }
+    }
+
+
+    @Test
+    @DisplayName("상품명 또는 상품 상세설명 조회 테스트")
+    public void findByItemNmOrItemDetail() {
+        List<Item> itemList = itemRepository.findByItemNmOrItemDetail("테스트 상품", "테스트 상품 상세 설명7");
+
+        for(Item item:itemList){
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("상품가격이 10, 000원 이하 상품 조회 테스트")
+    public void findByPriceLessThanEqualTest() {
+        List<Item> itemList = itemRepository.findByPriceLessThanEqual(10000);
+
+        for(Item item:itemList){
+            System.out.println(item.toString());
+        }
+    }
+
+
 
 }
